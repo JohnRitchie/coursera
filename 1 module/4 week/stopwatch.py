fb0d674b40f705a4e09f0272c7847fbba82a217d
JohnRitchie <lasarenko91@mail.ru>
2020-11-16 19:46:25 +0300

    stage

--- 1 module/4 week/stopwatch.py
+++ 1 module/4 week/stopwatch.py
@@ -1,4 +1,4 @@
-import simplegui
+import simpleguitk as simplegui
 
 counter = 0
 
@@ -6,8 +6,8 @@ counter = 0
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
 
-# def format(t):
-#     pass
+def format_time(time):
+    return time
 
 
 def timer_start():
@@ -24,6 +24,7 @@ def timer_restart():
     timer.stop()
     global counter
     counter = 0
+    timer.start()
 
 
 def timer_handler():
@@ -32,16 +33,20 @@ def timer_handler():
     print counter
 
 
-# define draw handler
+def draw_handler(canvas):
+    current_time = format_time(counter)
+    canvas.draw_text(current_time, [200, 200], 48, "Red")
 
 
 frame = simplegui.create_frame("Stopwatch: The Game", 400, 400)
 frame.add_button("Start", timer_start, 100)
 frame.add_button("Stop", timer_stop, 100)
 frame.add_button("Restart", timer_restart, 100)
-frame.start()
+frame.set_draw_handler(draw_handler)
 
 timer = simplegui.create_timer(100, timer_handler)
+
+frame.start()
 
 
 # Please remember to review the grading rubric
