96ffdae4c5a6ccf6e6d4865c959fab821ebf6707
Sid91 <lasarenko91@mail.ru>
2021-01-05 19:10:35 +0300

    accelerate the ship

--- 2 module/8 week/Spaceship.py
+++ 2 module/8 week/Spaceship.py
@@ -119,6 +119,13 @@ class Ship:
 
         self.angle += self.angle_vel
 
+        deceleration = 0.1
+        if self.thrust:
+            forward_vector = angle_to_vector(self.angle)
+            self.vel[0] += (forward_vector[0] * deceleration)
+            self.vel[1] += (forward_vector[1] * deceleration)
+
+
     def update_angle_vel(self, orientation):
         if orientation == "left":
             self.angle_vel -= 0.3
