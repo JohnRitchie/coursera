28fd7b70e3c056ae5ccffbbc4de48b83e6523c17
JohnRitchie <lasarenko91@mail.ru>
2021-02-01 21:03:56 +0300

    second ver

--- 3 module/merge.py
+++ 3 module/merge.py
@@ -10,6 +10,7 @@ def merge(line):
     # replace with your code
 
     tmp_list = [0 for _ in range(len(line))]
+
     result = [0 for _ in range(len(line))]
 
     index_list = 0
@@ -18,18 +19,25 @@ def merge(line):
             tmp_list[index_list] = number
             index_list += 1
 
-    for number in tmp_list:
-        number_index = tmp_list.index(number)
+    print tmp_list
 
-        if number_index == (len(tmp_list) - 1):
+    for indx in range(len(tmp_list)):
+        number_index = -1 - indx
+        if number_index + len(tmp_list) == 0:
             break
+        number = tmp_list[number_index]
 
-        next_number_index = number_index + 1
+        next_number_index = number_index - 1
         next_number = tmp_list[next_number_index]
 
         if number == next_number:
             tmp_list[number_index] = number + next_number
             tmp_list[next_number_index] = 0
+
+        print tmp_list
+
+    tmp_list.reverse()
+    print tmp_list
 
     index_list = 0
     for number in tmp_list:
@@ -38,3 +46,8 @@ def merge(line):
             index_list += 1
 
     return result
+
+
+# l = [8, 16]
+l = [8, 16]
+print merge(l)
