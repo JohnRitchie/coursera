c6e317f85f658c519284c020cfeef608a7e87f36
JohnRitchie <lasarenko91@mail.ru>
2021-02-01 23:02:44 +0300

    2.5 ver

--- 3 module/merge.py
+++ 3 module/merge.py
@@ -9,45 +9,50 @@ def merge(line):
     """
     # replace with your code
 
+    # result = [0 for _ in range(len(line))]
+
+    copy_line = line[:]
     tmp_list = [0 for _ in range(len(line))]
 
-    result = [0 for _ in range(len(line))]
-
     index_list = 0
-    for number in line:
+    for number in copy_line:
         if number > 0:
             tmp_list[index_list] = number
             index_list += 1
 
-    print tmp_list
+    for indx in range(len(line)):
+        index_list = 0
+        for number in tmp_list:
+            if number > 0:
+                tmp_list[index_list] = number
+                index_list += 1
 
-    for indx in range(len(tmp_list)):
-        number_index = -1 - indx
-        if number_index + len(tmp_list) == 0:
+        if indx == (len(line) - 1):
             break
-        number = tmp_list[number_index]
 
-        next_number_index = number_index - 1
+        number = tmp_list[indx]
+        next_number_index = indx + 1
         next_number = tmp_list[next_number_index]
 
+        print tmp_list
+
         if number == next_number:
-            tmp_list[number_index] = number + next_number
+            tmp_list[indx] = number + next_number
             tmp_list[next_number_index] = 0
 
         print tmp_list
 
-    tmp_list.reverse()
-    print tmp_list
+    # index_list = 0
+    # for number in tmp_list:
+    #     if number > 0:
+    #         result[index_list] = number
+    #         index_list += 1
 
-    index_list = 0
-    for number in tmp_list:
-        if number > 0:
-            result[index_list] = number
-            index_list += 1
-
-    return result
+    return tmp_list
 
 
 # l = [8, 16]
-l = [8, 16]
+# 8, 8, 8, 2 => 8, 16, 2, 0
+# 4, 4, 8, 8 => 8, 16, 0, 0
+l = [4, 4, 8, 8]
 print merge(l)
