b8878dcd9e4161f0b4622ca531a88ba174eb1905
JohnRitchie <lasarenko91@mail.ru>
2021-02-23 17:27:42 +0300

    frame

--- 3 module/3 week/TicTacToe.py
+++ 3 module/3 week/TicTacToe.py
@@ -13,7 +13,21 @@ NTRIALS = 1  # Number of trials to run
 SCORE_CURRENT = 1.0  # Score for squares played by the current player
 SCORE_OTHER = 1.0  # Score for squares played by the other player
 
-# Add your functions here.
+
+def mc_trial(board, player):
+    pass
+
+
+def mc_update_scores(scores, board, player):
+    pass
+
+
+def get_best_move(board, scores):
+    pass
+
+
+def mc_move(board, player, trials):
+    pass
 
 
 # Test game with the console or the GUI.  Uncomment whichever
