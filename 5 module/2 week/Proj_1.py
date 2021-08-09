631c896c268caf0bf3c7fd46368325b6aba83c01
JohnRitchie <lasarenko91@mail.ru>
2021-08-09 22:39:38 +0300

    template

--- 5 module/2 week/Proj_1.py
+++ 5 module/2 week/Proj_1.py
@@ -7,3 +7,27 @@ EX_GRAPH1 = {0: {1, 4, 5}, 1: {2, 6}, 2: {3}, 3: {0}, 4: {1},
              5: {2}, 6: {}}
 EX_GRAPH2 = {0: {1, 4, 5}, 1: {2, 6}, 2: {3, 7}, 3: {7}, 4: {1},
              5: {2}, 6: {}, 7: {3}, 8: {1, 2}, 9: {0, 3, 4, 5, 6, 7}}
+
+
+def make_complete_graph(num_nodes):
+    """
+    :param num_nodes:
+    :return:
+    """
+    return
+
+
+def compute_in_degrees(digraph):
+    """
+    :param digraph:
+    :return:
+    """
+    return
+
+
+def in_degree_distribution(digraph):
+    """
+    :param digraph:
+    :return:
+    """
+    return
