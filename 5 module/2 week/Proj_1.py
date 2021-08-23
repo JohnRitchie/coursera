8c9f3bc376df40995cd1b58d8d4f0aa45e621609
JohnRitchie <lasarenko91@mail.ru>
2021-08-23 22:57:05 +0300

    fx

--- 5 module/2 week/Proj_1.py
+++ 5 module/2 week/Proj_1.py
@@ -1,11 +1,11 @@
 """
  compute information about the distribution of the in-degrees for nodes in the graphs
 """
-EX_GRAPH0 = {0: {1, 2}, 1: {}, 2: {}}
+EX_GRAPH0 = {0: {1, 2}, 1: set([]), 2: set([])}
 EX_GRAPH1 = {0: {1, 4, 5}, 1: {2, 6}, 2: {3}, 3: {0}, 4: {1},
-             5: {2}, 6: {}}
+             5: {2}, 6: set([])}
 EX_GRAPH2 = {0: {1, 4, 5}, 1: {2, 6}, 2: {3, 7}, 3: {7}, 4: {1},
-             5: {2}, 6: {}, 7: {3}, 8: {1, 2}, 9: {0, 3, 4, 5, 6, 7}}
+             5: {2}, 6: set([]), 7: {3}, 8: {1, 2}, 9: {0, 3, 4, 5, 6, 7}}
 
 
 def make_complete_graph(num_nodes):
