4df7b29c4046dabe48cd66a5a2849826002c6502
JohnRitchie <30839417+JohnRitchie@users.noreply.github.com>
2021-09-15 21:39:23 +0300

    App 1.1 (#20)
    * start
    
    * EX_GRAPHS
    
    * template
    
    * simple draw graph
    
    * make_complete_graph description
    
    * make_complete_graph
    
    * refactor make_complete_graph
    
    * compute_in_degrees
    
    * in_degree_distribution
    
    * fx
    
    * fx
    
    * App_1 start
    
    * Provided code
    
    * normalized_distribution
    
    * stage
    
    * fx normalized_distribution
    
    * refactor compute_in_degrees
    
    * stage
    
    * refactor
    
    * stage
    
    * fx
    
    * make_plot in other file
    
    * fx make_plot
    
    * Question 2 start
    
    * fx make_plot
    
    * Question 2 stage
    
    * Question 2 complete
    
    * Question 4 start
    
    * fx
    
    * DPATrial template
    
    * Question 4 complete
    
    * Question 4 fx

---
+++ 5 module/2 week/application.py
@@ -0,0 +1,100 @@
+"""
+Provided code for Application portion of Module 1
+Imports physics citation graph
+"""
+
+import urllib2
+import networkx as nx
+from itertools import combinations
+import random
+import SimpleGUICS2Pygame.codeskulptor as codeskulptor
+import distribution
+import plot_graph
+import dpa_trial
+
+codeskulptor.set_timeout(20)
+
+###################################
+# Question 1
+
+CITATION_URL = "http://storage.googleapis.com/codeskulptor-alg/alg_phys-cite.txt"
+
+
+def load_graph(graph_url):
+    """
+    Function that loads a graph given the URL
+    for a text representation of the graph
+
+    Returns a dictionary that models a graph
+    """
+    graph_file = urllib2.urlopen(graph_url)
+    graph_text = graph_file.read()
+    graph_lines = graph_text.split('\n')
+    graph_lines = graph_lines[: -1]
+
+    print "Loaded graph with", len(graph_lines), "nodes"
+
+    answer_graph = {}
+    for line in graph_lines:
+        neighbors = line.split(' ')
+        node = int(neighbors[0])
+        answer_graph[node] = set([])
+        for neighbor in neighbors[1: -1]:
+            answer_graph[node].add(int(neighbor))
+
+    return answer_graph
+
+
+# citation_graph = load_graph(CITATION_URL)
+# disted_graph = distribution.in_degree_distribution(citation_graph)
+# normalized_disted_graph = distribution.normalized_distribution(disted_graph)
+# plot_graph.make_plot(normalized_disted_graph, 'In degree distribution of citation in scientific articles',
+#                      'Number of citation', 'Distribution of citation')
+
+###################################
+# Question 2
+
+
+def erdos_renyi(n, p):
+    vertex = set([v for v in range(n)])
+    edge = set()
+    for combination in combinations(vertex, 2):
+        a = random.random()
+        if a < p:
+            edge.add(combination)
+
+    graph = nx.Graph()
+    graph.add_nodes_from(vertex)
+    graph.add_edges_from(edge)
+
+    return graph
+
+
+# random_graph = erdos_renyi(27770, 0.0005)
+# disted_graph = distribution.in_degree_distribution(random_graph)
+# normalized_disted_graph = distribution.normalized_distribution(disted_graph)
+# plot_graph.make_plot(normalized_disted_graph, 'In degree distribution for the erdos renyi graph',
+#                      'Number of edges (log)', 'Fraction of nodes (log)')
+# plot_graph.make_plot(normalized_disted_graph, 'In degree distribution for the erdos renyi graph',
+#                      'Number of edges', 'Fraction of nodes', False)
+
+###################################
+# Question 4
+def dpa_graph(n, m):
+
+    complete_graph = distribution.make_complete_graph(m)
+    dpa = dpa_trial.DPATrial(m)
+
+    for i in range(m, n):
+        complete_graph[i] = dpa.run_trial(m)
+
+    return complete_graph
+
+
+# random_graph = dpa_graph(27770, 12)
+# disted_graph = distribution.in_degree_distribution(random_graph)
+# normalized_disted_graph = distribution.normalized_distribution(disted_graph)
+# plot_graph.make_plot(normalized_disted_graph, 'In degree distribution for the DPA graph',
+#                      'Number of edges (log)', 'Fraction of nodes (log)')
+# plot_graph.make_plot(normalized_disted_graph, 'In degree distribution for the DPA graph',
+#                      'Number of edges', 'Fraction of nodes', False)
---
+++ 5 module/2 week/distribution.py
@@ -0,0 +1,60 @@
+"""
+ compute information about the distribution of the in-degrees for nodes in the graphs
+"""
+EX_GRAPH0 = {0: {1, 2}, 1: set([]), 2: set([])}
+EX_GRAPH1 = {0: {1, 4, 5}, 1: {2, 6}, 2: {3}, 3: {0}, 4: {1},
+             5: {2}, 6: set([])}
+EX_GRAPH2 = {0: {1, 4, 5}, 1: {2, 6}, 2: {3, 7}, 3: {7}, 4: {1},
+             5: {2}, 6: set([]), 7: {3}, 8: {1, 2}, 9: {0, 3, 4, 5, 6, 7}}
+
+
+def make_complete_graph(num_nodes):
+    """
+    make a dictionary
+    corresponding to a complete directed graph
+    with the specified number of nodes
+    """
+    complete_graph = {}
+
+    for node in range(num_nodes):
+        complete_graph[node] = {i for i in range(num_nodes) if i != node}
+
+    return complete_graph
+
+
+def compute_in_degrees(digraph):
+    """
+    computes the in-degrees for the nodes in the graph
+    """
+    in_degrees = {node: 0 for node in digraph}
+
+    for node in digraph:
+        for value in digraph[node]:
+            in_degrees[value] += 1
+
+    return in_degrees
+
+
+def in_degree_distribution(digraph):
+    """
+    computes the unnormalized distribution of the in-degrees of the graph
+    """
+    distribution = {}
+    indegrees = compute_in_degrees(digraph).values()
+
+    for degree in indegrees:
+        if degree not in distribution.keys():
+            distribution[degree] = 1
+        else:
+            distribution[degree] += 1
+
+    return distribution
+
+
+def normalized_distribution(digraph):
+    total = sum(digraph.values())
+    distribution = {}
+    for node in digraph:
+        distribution[node] = (float(digraph[node])) / float(total)
+
+    return distribution
