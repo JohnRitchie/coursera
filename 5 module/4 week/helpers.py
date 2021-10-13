dfaadeaf785e1ab02351c87e44f70adf1030b0b0
JohnRitchie <lasarenko91@mail.ru>
2021-10-13 20:16:33 +0300

    bfs_visited

--- 5 module/4 week/helpers.py
+++ 5 module/4 week/helpers.py
@@ -2,4 +2,23 @@ from collections import deque
 
 
 def bfs_visited(ugraph, start_node):
-    pass
+    assert start_node in ugraph, 'Start node not in graph!'
+
+    queue = deque()
+    visited = {start_node}
+    queue.append(start_node)
+
+    while queue:
+        node = queue.popleft()
+        for neighbor in ugraph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+    return visited
+
+
+# EX_GRAPH1 = {0: {1, 4, 5}, 1: {2, 6}, 2: {3}, 3: {0}, 4: {1},
+#              5: {2}, 6: set([])}
+#
+# print bfs_visited(EX_GRAPH1, 5)
