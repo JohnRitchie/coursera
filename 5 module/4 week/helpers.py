01188a555c179dbc3b10a85c4aa77d71f0edf3b4
JohnRitchie <lasarenko91@mail.ru>
2021-10-13 22:50:40 +0300

    cc_visited

--- 5 module/4 week/helpers.py
+++ 5 module/4 week/helpers.py
@@ -1,4 +1,5 @@
 from collections import deque
+import random
 
 
 def bfs_visited(ugraph, start_node):
@@ -22,3 +23,22 @@ def bfs_visited(ugraph, start_node):
 #              5: {2}, 6: set([])}
 #
 # print bfs_visited(EX_GRAPH1, 5)
+
+
+def cc_visited(ugraph):
+    remain_node = set(ugraph.keys())
+    component_list = []
+
+    while remain_node:
+        node = random.choice(tuple(remain_node))
+        visited = bfs_visited(ugraph, node)
+        component_list.append(visited)
+        remain_node = remain_node - visited
+
+    return component_list
+
+
+# EX_GRAPH1 = {0: {1, 4, 5}, 1: {2, 6}, 2: {3}, 3: {0}, 4: {1},
+#              5: {2}, 6: set([])}
+#
+# print cc_visited(EX_GRAPH1)
