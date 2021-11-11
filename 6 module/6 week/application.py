79a4bf5aa54427e802c70264858e4162b46841e4
JohnRitchie <lasarenko91@mail.ru>
2021-11-11 19:17:13 +0300

    Question 10

--- 6 module/6 week/application.py
+++ 6 module/6 week/application.py
@@ -64,7 +64,7 @@ def compute_distortion(cluster_list, cluster_table):
 
 
 def make_singleton_list(cluster_table):
-    s_list = []
+    s_list = list()
 
     for line in cluster_table:
         s_list.append(alg_cluster.Cluster(set([line[0]]), line[1], line[2], line[3], line[4]))
@@ -72,13 +72,37 @@ def make_singleton_list(cluster_table):
     return s_list
 
 
+# data_table = alg_project3_viz.load_data_table(alg_project3_viz.DATA_111_URL)
+#
+# singleton_list = make_singleton_list(data_table)
+# cluster_list_hierarchical = proj.hierarchical_clustering(singleton_list, 9)
+# print compute_distortion(cluster_list_hierarchical, data_table)
+#
+# singleton_list = make_singleton_list(data_table)
+# cluster_list_kmeans = proj.kmeans_clustering(singleton_list, 9, 5)
+# print compute_distortion(cluster_list_kmeans, data_table)
+
+###################################
+# Question 10
 data_table = alg_project3_viz.load_data_table(alg_project3_viz.DATA_111_URL)
+num_clusters = range(6, 21)
+kmeans_distortion_list = []
+hierarchical_distortion_list = []
 
-singleton_list = make_singleton_list(data_table)
-cluster_list_hierarchical = proj.hierarchical_clustering(singleton_list, 9)
-print compute_distortion(cluster_list_hierarchical, data_table)
+for clusters in num_clusters:
+    singleton_list = make_singleton_list(data_table)
+    distortion = compute_distortion(proj.kmeans_clustering(singleton_list, clusters, 5), data_table)
+    kmeans_distortion_list.append(distortion/(10**10))
 
-singleton_list = make_singleton_list(data_table)
-cluster_list_kmeans = proj.kmeans_clustering(singleton_list, 9, 5)
-print compute_distortion(cluster_list_kmeans, data_table)
+    singleton_list = make_singleton_list(data_table)
+    distortion = compute_distortion(proj.hierarchical_clustering(singleton_list, clusters), data_table)
+    hierarchical_distortion_list.append(distortion/(10**10))
 
+plt.plot(num_clusters, kmeans_distortion_list, '-y', label='kmeans_clustering')
+plt.plot(num_clusters, hierarchical_distortion_list, '-g', label='hierarchical_clustering')
+plt.grid(which="major", linestyle="--", color="gray", linewidth=0.8)
+plt.legend(loc='upper left')
+plt.title("Distortion (111 points)")
+plt.ylabel("Distortion / 10**10")
+plt.xlabel("num_clusters")
+plt.show()
